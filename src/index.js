be1c7970a8edf5151767bfa70b72458da09d5e65
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-01 21:30:28 +0200

    setup firebase

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { firebase, FieldValue } from './lib/firebaseconfig'
+import FirebaseContext from './context/firebase'
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
