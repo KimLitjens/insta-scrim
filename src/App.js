aae70dd532b35c24dc164e1465f86b0e347c022f
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-01 20:44:10 +0200

    added tailwind, firebase and start images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Kims Instagram Start</h1>
+      <h1 className="text-red-500">Kims Instagram Start</h1>
     </div>
   );
 }
 
 export default App;
+
