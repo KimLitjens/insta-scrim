ee59da82c3442534bf42c043c3740fdbf2f6b58e
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-05 13:14:50 +0200

    add routes, pages & deleted some css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import './App.css';
 
 
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/signup'));
+const Profile = lazy(() => import('./pages/profile'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
-function App() {
-  return (
-    <div>
-      <h1 className="text-red-500">Kims Instagram Start</h1>
-    </div>
-  );
+export default function App() {
+  return (<p>Kim's instagram</p>)
 }
-
-export default App;
 
