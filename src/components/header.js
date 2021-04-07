fc1b1c4d15b6f2bb103a0ad63526663234953823
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-07 17:00:23 +0200

    add login/logout/signin to header

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,9 @@ import * as ROUTES from '../constants/routes'
 import FirebaseContext from '../context/firebase'
 
 export default function Header() {
+    const { firebase } = useContext(FirebaseContext)
+    const user = null
+
     return (
         <header className="h-16 bg-white border-b mb-8">
             <div className="container mx-auto max-width-lg h-full">
@@ -14,6 +17,54 @@ export default function Header() {
                                 <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
                             </Link>
                         </h1>
+                    </div>
+                    <div className="text-gray text-center flex items-center align-items">
+                        {user ? (
+                            <>
+                                <Link to={ROUTES.DASHBOARD} arial-label="Home">
+                                    <p>Dashboard</p>
+                                </Link>
+                                <button
+                                    type="button"
+                                    title="SIgn Out"
+                                    onClick={() => firebase.auth().signOut()}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            firebase.auth().signOut();
+                                        }
+                                    }}
+                                >
+                                    Sign Out
+                                </button>
+                                <div className="flex items-center cursor-pointer">
+                                    <Link to={`/p/${user.displayName}`}>
+                                        <img
+                                            className="rounded-full h-8 w-8 flex"
+                                            src={`/images/avatars/${user.displayName}.jpg`}
+                                            alt={`${user.displayName} profile picture`}
+                                        />
+                                    </Link>
+                                </div>
+                            </>
+                        ) : (
+                                <>
+                                    <Link to={ROUTES.LOGIN}>
+                                        <button
+                                            type="button"
+                                            className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8">
+                                            Log In
+                                        </button>
+                                    </Link>
+                                    <Link to={ROUTES.SIGN_UP}>
+                                        <button
+                                            type="button"
+                                            className="font-bold text-sm rounded text-blue w-20 h-8"
+                                        >
+                                            Sign Up
+                                        </button>
+                                    </Link>
+                                </>
+                            )}
                     </div>
                 </div>
             </div>
