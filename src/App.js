4e09127fea91ba728ee66a353d2befa1fe7a7877
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-08 18:14:47 +0200

    add use-auth-listener

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import UserContext from './context/user'
+import useAuthListener from './hooks/use-auth-listener'
 import * as ROUTES from './constants/routes'
 
 const Dashboard = lazy(() => import('./pages/dashboard'));
@@ -11,7 +12,7 @@ const Profile = lazy(() => import('./pages/profile'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
-  const user = null
+  const { user } = useAuthListener()
   return (
     <UserContext.Provider value={{ user }} >
       <Router>
