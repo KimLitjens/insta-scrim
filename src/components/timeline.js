8bd84a1a0cdfa8ccb036b3bd41042112e1c3981f
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-10 16:44:34 +0200

    add follewed-fotos, changed firebase and timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,18 @@
-import REACT from 'react'
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import useFollwedUsersPhotos from '../hooks/use-followed-users-photos'
 
 export default function Timeline() {
-    return (<p>I'm the timeline</p> )
+    const { photos } = useFollwedUsersPhotos
+    console.log('wokring')
+
+    return (
+        <div className="container col-span-2">
+            {!photos ? (
+                <Skeleton count={4} width={640} height={500} className="mb-5" />
+            ) : (
+                    photos.map((content) => <p>I will be a photo!</p>)
+                )}
+        </div>
+    );
 }
