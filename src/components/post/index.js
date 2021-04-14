83285d805fc7d5ba64c61381ccef0333ccc04c2b
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-14 16:29:47 +0200

    add post footer & actions

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,3 +1,5 @@
+import { useRef } from 'react'
+
 import Header from './header';
 import Image from './image';
 import Actions from './actions';
@@ -5,9 +7,19 @@ import Footer from './footer';
 import Comments from './comments';
 
 export default function Post({ content }) {
+    const commentInput = useRef(null)
+    const handleFocus = () => commentInput.current.focus()
+
     return (
         <div className="rounded col-span-4 border bg-white-100 mb-16">
             <Image src={content.imageSrc} caption={content.caption} />
+            <Footer username={content.username} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
         </div>
     )
 }
