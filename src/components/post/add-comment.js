2df4e954b4213b78d7936a1e7c6dfa1dd7d3d8ad
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-19 09:15:26 +0200

    add handlesubmitcomment

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -26,7 +26,34 @@ export default function AddComment({ docId, comments, setComments, commentInput 
 
     return (
         <div className="border-t border-gray">
-            <p>I will be a form!</p>
+            <form
+                className="flex w-full justify-between pl-0 pr-5"
+                onSubmit={(event) =>
+                    comment.length >= 3 ? handleSubmitComment(event) : event.preventDefault
+                }
+                method="POST"
+            >
+                <input
+                    aria-label="Add a comment"
+                    autoComplete="off"
+                    className="text-sm text-gray w-full mr-3 py-5 px-4"
+                    type="text"
+                    name="add-comment"
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={({ target }) => setComment(target.value)}
+                    ref={commentInput}
+                />
+                <button
+                    className={`text-sm font-bold text-blue-500 ${!comment && 'opacity-25'}`}
+                    type="button"
+                    disabled={comment.length < 3}
+                    onClick={handleSubmitComment}
+                >
+                    Post
+                </button>
+            </form>
+
         </div>
     );
 }
