4c984ea81461d716a51ef85edcf199a96664b21e
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-27 09:37:26 +0200

    add toggle follow

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -132,3 +132,15 @@ export async function getUserPhotosByUsername(username) {
 
     return photos;
 }
+
+export async function toggleFollow(
+    isFollowingProfile,
+    activeUserDocId,
+    profileDocId,
+    profileId,
+    followingUserId
+) {
+    await updateUserFollowing(activeUserDocId, profileId, isFollowingProfile);
+    await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
+
+}
