c31dde52c64612834af7a8f7d0187c643d8b49be
Kim Litjens <Kimlitjens4@gmail.com>
2021-04-28 10:36:13 +0200

    fixed bug

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -7,13 +7,13 @@ export default function Header({
     photosCount,
     followerCount: followers,
     setFollowerCount,
-    username,
+    username: profileUsername,
     profile: { docId: profileDocId, userId: profileUserId, fullName, following = [] }
 }) {
     const { user } = useUser();
     const [isFollowingProfile, setIsFollowingProfile] = useState();
     console.log("🚀 ~ file: header.js ~ line 15 ~ isFollowingProfile", isFollowingProfile)
-    const activeBtnFollow = user.username && user.username !== username;
+    const activeBtnFollow = user && user.username && user.username !== profileUsername;
 
     const handleToggleFollow = async () => {
         setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
@@ -37,14 +37,14 @@ export default function Header({
         <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
             <div className="container flex justify-center">
                 <img
-                    src={`/images/avatars/${username}.jpg`}
-                    alt={`${username} profile`}
+                    src={`/images/avatars/${profileUsername}.jpg`}
+                    alt={`${profileUsername} profile`}
                     className="rounded-full h-40 w-40 flex"
                 />
             </div>
             <div className="flex items-center justify-center flex-col col-span-2">
                 <div className="container flex items-center">
-                    <p className="text-2xl mr-4">{username}</p>
+                    <p className="text-2xl mr-4">{profileUsername}</p>
                     {activeBtnFollow && (
                         <button
                             className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
